f98175241574e541b3088bab30e1b8c02fc76bc2
jyadim <ahmadrakandzaky.a@gmail.com>
2025-01-16 14:08:02 +0700

    proses detail

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -7,10 +7,17 @@ use Illuminate\Http\Request;
 
 class AlbumController extends Controller
 {
-    public function index($slug){
-        $photo = Photo::where('slug', $slug)
-            ->with('id')
-            ->firstOrFail();
-        return view('detailPicture', compact('photo'));
-    }
+    public function index($slug)
+{
+    // Log or debug the incoming slug
+    \Log::info("Slug received: " . $slug);
+
+    $photo = Photo::where('slug', $slug)->get();
+
+    // Log or debug the photo data
+    \Log::info($photo);
+
+    return view('detailPicture', compact('photo'));
+}
+
 }
